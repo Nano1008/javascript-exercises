2c8bb649d973aba8290a085f2a66b549fdf27340
Sinan <nanossn1008@gmail.com>
2024-12-28 00:20:15 +0000

    Finish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    // check if they are strings
+    if (typeof num1 == "string" || typeof num2 == "string") {
+        return "ERROR";
+    }
 
+    // check if they are posivite integers
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
+
+    const min = Math.min(num1, num2);
+    const max = Math.max(num1, num2);
+    let sumResult = 0;
+    for (let start = min; start <= max; start++) {
+        sumResult += start;
+    }
+    return sumResult;
 };
 
 // Do not edit below this line

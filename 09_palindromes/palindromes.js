526058a886eb315dfd7fb0d14c0d6dcc28c8db50
Sinan <nanossn1008@gmail.com>
2025-01-06 23:13:56 +0000

    Finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Remove punctuation and word break
+    const cleanStr = str.toLowerCase().replace(/[^\w]/g, "");
 
+    const charArr = cleanStr.split("");
+    const reversed = charArr.slice().reverse();
+    return charArr.join("") === reversed.join("");
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;

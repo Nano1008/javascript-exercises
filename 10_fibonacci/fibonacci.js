7d38c9d271d2df7038bdf7cc51e5544c4653f7b3
Sinan <nanossn1008@gmail.com>
2025-01-06 23:32:21 +0000

    Finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    const num = Number(n);
+    if (num < 0) {
+        return "OOPS";
+    } else if (num == 0) {
+        return 0;
+    } else if (num == 1) {
+        return 1;
+    } else if (num == 2) {
+        return 1;
+    }
 
+    const arr = new Array(num);
+    arr[0] = 1;
+    arr[1] = 1;
+    for (let i = 2; i < num; i++) {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+
+    return arr[num - 1];
 };
 
 // Do not edit below this line
